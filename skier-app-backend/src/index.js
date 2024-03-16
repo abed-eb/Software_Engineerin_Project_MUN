05fcf6897047df581863c616d61d650b17792e3e
faham1997 <nafidfaham08@gmail.com>
2024-03-16 03:52:46 -0230

    project-backend: Create routes for location route

--- skier-app-backend/src/index.js
+++ skier-app-backend/src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const mapRoute = require("./routes/map.routes");
+const locationRoute = require("./routes/location.routes");
 
 const db = require("./config/db");
 db();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 //Use Routes
 app.use("/api/v1/map", mapRoute);
+app.use("/api/v1/location", locationRoute);
 
 app.get("/", (req, res) => {
   res.json("Server is running successfully!");
